594fef0e26b77b6825992ef8f04e791f3de10e74
Wuzzaza <wzln@mail.ru>
2017-05-14 12:48:40 +0300

    First Commit

---
+++ MyFirstApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MyFirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numbersTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // numbersTextBox
+            // 
+            this.numbersTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numbersTextBox.Location = new System.Drawing.Point(12, 12);
+            this.numbersTextBox.Name = "numbersTextBox";
+            this.numbersTextBox.Size = new System.Drawing.Size(224, 38);
+            this.numbersTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.numbersTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numbersTextBox;
+    }
+}
+
---
+++ MyFirstApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyFirstApp
+{
+    public partial class Form1 : Form
+    {
+        string lastOperand;
+        int lastNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                Button btn = new Button();
+                btn.Text = (i + 1).ToString();
+                btn.Location = new Point(10 + (i % 3) * 50, 120 + (i / 3) * 50);
+                btn.Size = new Size(50, 50);
+
+                btn.Click += (object sender, EventArgs e) => { numbersTextBox.Text += (sender as Button).Text; };
+
+                this.Controls.Add(btn);
+
+            }
+
+
+
+            string[] opratorButtonsName = { "+", "-", "*", "/" };
+            for (int i = 0; i < 4; i++)
+            { 
+                Button btn = new Button();
+                btn.Text = opratorButtonsName[i];
+                btn.Location = new Point(160, 70 + i * 50);
+                btn.Size = new Size(50, 50);
+                this.Controls.Add(btn);
+
+                btn.Click += (object sender, EventArgs e) => {
+                    int.TryParse(numbersTextBox.Text, out lastNumber);
+                    lastOperand = (sender as Button).Text;
+
+                };
+
+            }
+
+        }
+                
+    }
+}
